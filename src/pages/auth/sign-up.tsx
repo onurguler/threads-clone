07b3381708f5cf64247f46f3b011157d35d9e54a
onurguler <guleronurosman@gmail.com>
2023-07-08 13:21:54 +0300

    light theme version of sign in & sign up pages

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function SignUpPage() {
   return (
-    <section className="grid h-screen w-full place-items-center bg-gradient-to-br from-zinc-950 to-slate-950 p-6">
+    <section className="grid h-screen w-full place-items-center bg-gradient-to-br from-zinc-100 to-slate-200 p-6 dark:from-zinc-950 dark:to-slate-950">
       <div className="mx-auto flex w-full max-w-sm flex-col space-y-4 backdrop-blur-3xl">
         <div className="flex items-center gap-2 ">
           <span className="bg-gradient-to-r from-pink-500 to-yellow-500 bg-clip-text text-4xl font-extrabold text-transparent">
@@ -13,31 +13,31 @@ export default function SignUpPage() {
         <p className="">Sign up to continue the platform.</p>
         <div className="flex flex-col space-y-4">
           <input
-            className="w-full rounded-lg border border-zinc-600 bg-zinc-800 px-4 py-2 outline-none"
+            className="w-full rounded-lg border border-zinc-300 bg-zinc-100 px-4 py-2 outline-none dark:border-zinc-600 dark:bg-zinc-800"
             type="text"
             placeholder="What's your name?"
           />
           <input
-            className="w-full rounded-lg border border-zinc-600 bg-zinc-800 px-4 py-2 outline-none"
+            className="w-full rounded-lg border border-zinc-300 bg-zinc-100 px-4 py-2 outline-none dark:border-zinc-600 dark:bg-zinc-800"
             type="text"
             placeholder="Please enter your email address"
           />
           <input
-            className="w-full rounded-lg border border-zinc-600 bg-zinc-800 px-4 py-2 outline-none"
+            className="w-full rounded-lg border border-zinc-300 bg-zinc-100 px-4 py-2 outline-none dark:border-zinc-600 dark:bg-zinc-800"
             type="text"
             placeholder="Username"
           />
           <input
-            className="w-full rounded-lg border border-zinc-600 bg-zinc-800 px-4 py-2 outline-none"
+            className="w-full rounded-lg border border-zinc-300 bg-zinc-100 px-4 py-2 outline-none dark:border-zinc-600 dark:bg-zinc-800"
             type="password"
             placeholder="Password"
           />
           <input
-            className="w-full rounded-lg border border-zinc-600 bg-zinc-800 px-4 py-2 outline-none"
+            className="w-full rounded-lg border border-zinc-300 bg-zinc-100 px-4 py-2 outline-none dark:border-zinc-600 dark:bg-zinc-800"
             type="password"
             placeholder="Confirm Password"
           />
-          <button className="rounded-full bg-blue-600 px-4 py-2 font-medium hover:bg-blue-500">
+          <button className="rounded-full bg-blue-600 px-4 py-2 font-medium text-zinc-100 hover:bg-blue-700 hover:dark:bg-blue-500">
             Sign Up
           </button>
           <p className="text-center text-sm text-zinc-600">
